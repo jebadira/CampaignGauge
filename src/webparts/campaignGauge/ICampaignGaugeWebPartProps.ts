aac8289498ca9b2507f599504295954de53741d5
jebadira <jebadira@asu.edu>
2017-09-14 14:45:59 -0700

    init

---
+++ src/webparts/campaignGauge/ICampaignGaugeWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface ICampaignGaugeWebPartProps {
+  description: string;
+}
---
+++ src/webparts/campaignGauge/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICampaignGaugeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CampaignGaugeWebPartStrings' {
+  const strings: ICampaignGaugeWebPartStrings;
+  export = strings;
+}
